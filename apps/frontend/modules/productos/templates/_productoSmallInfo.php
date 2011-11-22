013446b591f6713da27c2302b83489b2c9802df7
rsantellan@gmail.com <rsantellan@gmail.com@9fb6aa83-d424-9ad8-849c-5b560eaf926a>
2011-11-22 22:04:04 +0000

    Se agrego el detalle grande del producto.

--- apps/frontend/modules/productos/templates/_productoSmallInfo.php
+++ apps/frontend/modules/productos/templates/_productoSmallInfo.php
@@ -1,8 +1,11 @@
 <div class="producto_small_container">
-    <a href="javascript:void(0)">
+	
+    <a href="<?php echo url_for("@detalleProducto?categoria=".$slug."&id=".$producto->getId()."&slug=".$producto->getSlug()); ?>">
         <img src="<?php echo $producto->retrieveAvatar(array(mdWebOptions::WIDTH => 165, mdWebOptions::HEIGHT => 165,  mdWebOptions::CODE => mdWebCodes::RESIZECROP)); ?>" alt="<?php echo $producto->getName(); ?>"/>
     </a>
     <br class="spacer" />
-    <?php echo $producto; ?>    
+    <label>
+		<?php echo $producto; ?>    
+	</label>
 </div>
 
