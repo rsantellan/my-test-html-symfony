ce7651261906439b1129043f38fe67c388ab2c5c
rsantellan@gmail.com <rsantellan@gmail.com@9fb6aa83-d424-9ad8-849c-5b560eaf926a>
2011-11-27 00:08:44 +0000

    Se agregan los botones de las redes sociales.

--- apps/frontend/modules/default/templates/quienesSomosSuccess.php
+++ apps/frontend/modules/default/templates/quienesSomosSuccess.php
@@ -2,8 +2,22 @@
 
 <!--left start-->
 <div class="left">
-        <h2><span class="black"><?php echo __("quienesSomos_titulo izquierdo");?></span><span class="brown"><?php echo __("quienesSomos_titulo derecho");?></span></h2>
-	<p class="darkgrey">
+  <h2><span class="black"><?php echo __("quienesSomos_titulo izquierdo");?></span><span class="brown"><?php echo __("quienesSomos_titulo derecho");?></span></h2>
+	<div class="buttons_containers">
+    <div class="facebook">
+      <div id="fb-root"></div><script src="http://connect.facebook.net/en_US/all.js#appId=170247116382784&amp;xfbml=1"></script><fb:like href="<?php echo $sf_request->getUri(); ?>" send="false" layout="button_count" width="120" show_faces="false" font=""></fb:like>
+    </div>
+    <div class="twitter">
+      <a href="http://twitter.com/share" class="twitter-share-button" data-count="horizontal">Tweet</a>
+      <script type="text/javascript" src="http://platform.twitter.com/widgets.js"></script>
+    </div>
+    
+    <div class="gplusone">
+      <g:plusone size="medium" href="<?php echo $sf_request->getUri(); ?>"></g:plusone>
+    </div>
+  </div>
+  <br class="spacer" />
+  <p class="darkgrey">
             <?php
                 echo __("quienesSomos_texto");
             ?>
