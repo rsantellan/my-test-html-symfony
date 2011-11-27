639d0d70762c899edf28b1ccf7ccc210336f740b
rsantellan@gmail.com <rsantellan@gmail.com@9fb6aa83-d424-9ad8-849c-5b560eaf926a>
2011-11-27 00:08:44 +0000

    Se agregan los botones de las redes sociales.

--- trunk/apps/frontend/templates/layout.php
+++ trunk/apps/frontend/templates/layout.php
@@ -80,4 +80,14 @@
         </div>
         <!--footer end -->
     </body>
+<!-- Place this render call where appropriate -->
+<script type="text/javascript">
+  window.___gcfg = {lang: 'es'};
+
+  (function() {
+    var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+    po.src = 'https://apis.google.com/js/plusone.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+  })();
+</script>
 </html>
