4e1b966341fb50789e0983db6bee133732781165
rsantellan@gmail.com <rsantellan@gmail.com@9fb6aa83-d424-9ad8-849c-5b560eaf926a>
2011-11-28 11:04:28 +0000

    Se arreglo los estilos de los botones sociales. Se sacaron a un partial para poder incluirlos en todos lados.

--- trunk/apps/frontend/modules/default/templates/indexSuccess.php
+++ trunk/apps/frontend/modules/default/templates/indexSuccess.php
@@ -3,6 +3,7 @@
 <!--left start-->
 <div class="left">
   <h2><span class="black"><?php echo __("inicio_titulo izquierdo"); ?></span><span class="brown"><?php echo __("inicio_titulo derecho"); ?></span></h2>
+  <?php //include_partial("default/socialButtons"); ?>
   <p class="darkgrey">
 	<?php
 	echo __("inicio_bienvenidos");
