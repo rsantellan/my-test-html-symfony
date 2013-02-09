4b57e6e65832a75b44950961087ce1498d6c9e67
rsantellan@gmail.com <rsantellan@gmail.com@9fb6aa83-d424-9ad8-849c-5b560eaf926a>
2013-02-09 01:22:34 +0000

    Se agrega la parte de locaciones doble.
    Se subo para que se vea como deberia de quedar.

--- apps/frontend/modules/default/templates/_localInfo.php
+++ apps/frontend/modules/default/templates/_localInfo.php
@@ -2,14 +2,14 @@
 <br class="spacer" />
 <div class="left1">
     <div class="testimonials">
-        <h2 class="smallBlack"><span class="black2"><?php echo $profile->getValue("nombre"); ?></span></h2>
+        <h1 class="smallBlack"><span class="black2"><?php echo $profile->getValue("nombre"); ?></span></h1>
         <?php 
             $instance = mdMediaManager::getInstance(mdMediaManager::ALL, $local)->load(); 
             
             if($instance->hasAlbum("default")):
                 $avatar = $instance->getAvatar(NULL, true);
                 if(!is_null($avatar)):
-        ?>
+            ?>
         <div class="blueBg">
             <div class="blueBgText">
                 <p><?php echo __("locales_ver galeria");?></p>		
@@ -17,14 +17,14 @@
             
             <div class="blueBgPic">
                 <a class="grouped_images" rel="group<?php echo $local->getId();?>" href="<?php echo $avatar->getUrl(array(mdWebOptions::WIDTH => 640, mdWebOptions::HEIGHT => 480,  mdWebOptions::CODE => mdWebCodes::CROPRESIZE)); ?>" title="" >
-                    <img src="<?php echo $avatar->getUrl(array(mdWebOptions::WIDTH => 58, mdWebOptions::HEIGHT => 58,  mdWebOptions::CODE => mdWebCodes::CROPRESIZE)); ?>" alt="" />
+                    <img src="<?php echo $avatar->getUrl(array(mdWebOptions::WIDTH => 300, mdWebOptions::HEIGHT => 200,  mdWebOptions::CODE => mdWebCodes::CROPRESIZE)); ?>" alt="" />
                 </a>
             </div>
             <div class="hidden"> 
                 <?php foreach($instance->getItems() as $item): ?>
                     <?php if($item->getId() != $avatar->getId() ): ?>
                         <a class="grouped_images" rel="group<?php echo $local->getId();?>" href="<?php echo $item->getUrl(array(mdWebOptions::WIDTH => 640, mdWebOptions::HEIGHT => 480,  mdWebOptions::CODE => mdWebCodes::CROPRESIZE)); ?>" title="" >
-                            <img src="<?php echo $item->getUrl(array(mdWebOptions::WIDTH => 58, mdWebOptions::HEIGHT => 58,  mdWebOptions::CODE => mdWebCodes::CROPRESIZE)); ?>" alt="" />
+                            <img src="<?php echo $item->getUrl(array(mdWebOptions::WIDTH => 300, mdWebOptions::HEIGHT => 200,  mdWebOptions::CODE => mdWebCodes::CROPRESIZE)); ?>" alt="" />
                         </a>
                     <?php endif; ?>
                 <?php endforeach; ?>
@@ -39,9 +39,8 @@
             <li><strong><?php echo __("locales_Direccion:");?></strong><span><?php echo $profile->getValue("direccion"); ?></span></li>
             <li><strong><?php echo __("locales_Telefono:");?></strong><span><?php echo $profile->getValue("telefono"); ?></span></li>
             <li><strong><?php echo __("locales_Horario:");?></strong><span><?php echo $profile->getValue("horario"); ?></span></li>                
-            <li><strong><?php echo __("locales_Descripcion:");?></strong><span><?php echo $profile->getValue("descripcionLocal"); ?></span></li> 
+            <li><strong class="description"><?php echo __("locales_Descripcion:");?></strong><span><?php echo $profile->getValue("descripcionLocal"); ?></span></li> 
         </ul><br class="spacer" />
     </div>
 </div>
 <br class="spacer" />
-<hr/>
