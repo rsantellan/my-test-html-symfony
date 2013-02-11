8130a593a617b1387bf22e15198f1bbc380b1cd7
rsantellan <rsantellan@9fb6aa83-d424-9ad8-849c-5b560eaf926a>
2013-02-11 17:22:14 +0000

    Se agrega los codigos de google.

--- trunk/apps/frontend/templates/layout.php
+++ trunk/apps/frontend/templates/layout.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
     <head>
+        <meta name="google-site-verification" content="TUrdyEzGtMsF8gKtqns0GSzD6rHan9lQQxdFE5eEiFM" />
         <?php include_http_metas() ?>
         <?php include_metas() ?>
         <?php include_title() ?>
@@ -90,6 +91,20 @@
   fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));</script>
 
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-27401567-1']);
+  _gaq.push(['_setDomainName', 'naturalia.com.uy']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
     </body>
 <!-- Place this render call where appropriate -->
 </html>
